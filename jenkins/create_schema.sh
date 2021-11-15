2e9893e125a054a5e87026879a58082c7baf62bc
Ubuntu <harryvolker@harryvolker-jenkins.tloklhw0axoedn2mgazelmvune.cwx.internal.cloudapp.net>
2021-11-15 10:42:25 +0000

    removing jenkins files

--- jenkins/create_schema.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-source venv/bin/activate
-echo "Creating schema..."
-python3 create.py
-echo "Schema created!"
--- jenkins/test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-source venv/bin/activate
-python3 -m pytest \
-	--cov=application \
-    --cov-report term-missing \
-    --cov-report xml:coverage.xml \
-    --junitxml=junit_report.xml

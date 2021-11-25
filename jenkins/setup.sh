27467478a9f893bbe2a97b631429dba764608208
Ubuntu <susanadam@dev-jenkins.ybnbbtvjcqbudaom4odmkkmrlh.parx.internal.cloudapp.net>
2021-11-25 15:56:29 +0000

    setup update

--- jenkins/setup.sh
+++ jenkins/setup.sh
@@ -6,9 +6,11 @@ echo "Setup stage"
 sudo apt-get update
 sudo apt-get install -y curl jq
 
+
 #install docker
 
 curl https://get.docker.com | sudo bash
+sudo usermod -aG jenkins
 
 #install docker compose
 version=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | jq -r '.tag_name')

7fee0ced9141c32dad431cf4c94621698ba1706b
Ubuntu <susanadam@dev-jenkins.ybnbbtvjcqbudaom4odmkkmrlh.parx.internal.cloudapp.net>
2021-11-26 14:17:44 +0000

    deployment stage3

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -4,4 +4,6 @@ echo "Deploy stage"
 
 scp docker-compose.yaml jenkins@swarm-manager:/home/jenkins/docker-compose.yaml
 
-ssh jenkins@swarm-manager docker stack deploy --compose-file docker-compose.yaml todo-app
+ssh jenkins@swarm-manager \
+    DOCKER_HUB_CREDS_USR=$DOCKER_HUB_CREDS_USR \
+    docker stack deploy --compose-file docker-compose.yaml todo-app
